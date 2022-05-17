fbe79a273d15e16808975a83eaaf14fbb466db68
Andres Arevalo Fajardo <a.arevalof@uniandes.edu.co>
2022-05-16 20:49:42 -0500

    navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservaListComponent } from './reserva/reserva-list/reserva-list.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+];
+//{path : '',component:ReservaListComponent}
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
