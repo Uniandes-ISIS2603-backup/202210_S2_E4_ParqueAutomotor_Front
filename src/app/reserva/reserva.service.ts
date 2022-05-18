fcdc48e826afdd2b6df371874817a269a07d2a65
BraitoRX <69652100+BraitoRX@users.noreply.github.com>
2022-05-18 07:37:43 -0500

    cambios

--- src/app/reserva/reserva.service.ts
+++ src/app/reserva/reserva.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Reserva } from './Reserva';
 import { Observable } from 'rxjs';
+import { ReservaDetail } from './Reserva-detail';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,9 @@ constructor(private http: HttpClient) { }
 
 getReservas(): Observable<Reserva[]>{
   return this.http.get<Reserva[]>(this.apiUrl);
+}
+getReserva(id: string): Observable<ReservaDetail> {
+  return this.http.get<ReservaDetail>(this.apiUrl + "/" + id);
 }
 
 }
