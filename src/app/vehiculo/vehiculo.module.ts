2dbe1676f840c8642191d73ce1a1b4e69f41bdac
Eduardo Herrera <ej.herreraa@uniandes.edu.co>
2022-05-17 22:15:54 -0500

    vehiculos list

--- src/app/vehiculo/vehiculo.module.ts
+++ src/app/vehiculo/vehiculo.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiculoListComponent } from './vehiculo-list/vehiculo-list.component';
 import { VehiculoDetailComponent } from './vehiculo-detail/vehiculo-detail.component';
+import { RouterModule } from '@angular/router';
+import { ReservaRoutingModule } from '../reserva/reserva-routing.routing';
+import { VehiculoRoutingModule } from './vehiculo-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    VehiculoRoutingModule
   ],
   exports: [VehiculoListComponent],
   declarations: [VehiculoListComponent, VehiculoDetailComponent],
