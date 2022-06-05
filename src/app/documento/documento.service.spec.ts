dc60ea0765ae467e97d501109109ea99129189dc
BraitoRX <69652100+BraitoRX@users.noreply.github.com>
2022-06-05 01:40:14 -0500

    pruebas finalizadas

--- src/app/documento/documento.service.spec.ts
+++ src/app/documento/documento.service.spec.ts
@@ -1,11 +1,14 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { DocumentoService } from './documento.service';
 
 describe('Service: Documento', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,HttpClientModule],
       providers: [DocumentoService]
     });
   });
